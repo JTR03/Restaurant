762389a57542ed419a5616b6d43fdb296afa8b20
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-01-17 13:28:13 +0200

    collected data and added css

---
+++ src/data.js
@@ -0,0 +1,98 @@
+const data = [
+    {
+        id:1,
+        title: 'Pancakes',
+        category: 'breakfast',
+        price: 25.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:2,
+        title: 'Pancakes',
+        category: 'desserts',
+        price: 35.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:3,
+        title: 'Pancakes',
+        category: 'dinner',
+        price: 55.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:4,
+        title: 'Pancakes',
+        category: 'breakfast',
+        price: 25.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:5,
+        title: 'Ice cream',
+        category: 'dessert',
+        price: 15.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:6,
+        title: 'Pancakes',
+        category: 'dinner',
+        price: 25.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:7,
+        title: 'Pancakes',
+        category: 'breakfast',
+        price: 25.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:8,
+        title: 'Pancakes',
+        category: 'dessert',
+        price: 25.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:9,
+        title: 'Pancakes',
+        category: 'dinner',
+        price: 25.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:10,
+        title: 'Pancakes',
+        category: 'lunch',
+        price: 25.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:11,
+        title: 'Pancakes',
+        category: 'lunch',
+        price: 25.00,
+        img:' ',
+        description:'' 
+    },
+    {
+        id:12,
+        title: 'Pancakes',
+        category: 'lunch',
+        price: 25.00,
+        img:' ',
+        description:'' 
+    }
+]
